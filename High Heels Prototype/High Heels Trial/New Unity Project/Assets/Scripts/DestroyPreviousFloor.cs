11072466d3a88f5d3158652f572994107d09aaab
Osman-Br <56321113+Osman-Br@users.noreply.github.com>
2021-06-24 20:53:19 +0300

    import

---
+++ High Heels Prototype/High Heels Trial/New Unity Project/Assets/Scripts/DestroyPreviousFloor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPreviousFloor : MonoBehaviour  //destroy floors that touches the destroyPoint
+{
+    private BoxCollider bCollider;
+    private void Awake()
+    {
+        bCollider = GetComponent<BoxCollider>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            Destroy(other.gameObject);    
+        }
+    }
+}
